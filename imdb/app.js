23c5f3064a92c13a393ab59548d394ca296ebbe4
Vignesh-mediwave <vignesh@mindwaveventures.com>
2022-02-01 12:20:45 +0530

    read and update coding added

---
+++ imdb/app.js
@@ -0,0 +1,30 @@
+const movies = require("./movie");
+//add new movie
+const movieadd = { id: 104, name: "Vikings", genre: "war" };
+movies.push(movieadd);
+
+//find specfic movie
+const id = 100;
+const movieToFind = movies.find(function (m) {
+  return m.id == id;
+});
+if (!movieToFind) {
+  console.log(`movie with ${id} not found`);
+} else {
+  console.log("movie found", movieToFind);
+}
+
+//console.log(movies);
+
+//updatemovie
+
+const updateId = 101;
+const mName = "the-last-kingdom";
+
+const updateName = movies.findIndex((m) => m.id === updateId);
+if (updateName == -1) {
+  console.log(`id ${updateId} not found`);
+} else {
+  movies[updateName]["name"] = mName;
+}
+console.log(movies);
---
+++ imdb/movie.js
@@ -0,0 +1,7 @@
+const movies = [
+  { id: 100, name: "billions", genre: "drama" },
+  { id: 101, name: "avengers", genre: "sci - fi" },
+  { id: 102, name: "game_of_thrones", genre: "history" },
+];
+
+module.exports = movies;
