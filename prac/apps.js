2fa70ec4b738ef6574a6a5c67498b2b468c0062d
Vignesh-mediwave <vignesh@mindwaveventures.com>
2022-02-01 11:25:12 +0530

    intial api for imdb

---
+++ prac/apps.js
@@ -0,0 +1,8 @@
+const utils = require("./utils");
+const stringsUtils = require("./strings");
+
+const result = stringsUtils.upperCase("hello");
+console.log(result);
+
+const sum = utils.addNumbers(2, 3);
+console.log(sum);
---
+++ prac/strings.js
@@ -0,0 +1,1 @@
+exports.upperCase = (str) => str.toUpperCase();
